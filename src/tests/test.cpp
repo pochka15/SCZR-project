447672ccbd10e79bc7aa2ac039e8a91dc5716517
Artsiom <koshkov-artem@mail.ru>
2020-06-12 14:33:37 +0300

    Add a little test

--- src/tests/test.cpp
+++ src/tests/test.cpp
@@ -9,3 +9,18 @@ TEST(ShMem, singleThreadCorrectBytesReceived) {
     getData();
     ASSERT_TRUE(text == textFromSharedMemory());
 }
+
+TEST(ShMem, CHILD_PARENT_PROCESSES_CORRECTED_TEXT_RECEIVED) {
+    std::string text = std::string(4095, 'a');
+    int pid = fork();
+    if (-1 == pid) {
+        FAIL() << "fork error";
+    }
+    if (0 == pid) { // child
+        sendViaSharedMemory(text);
+        exit(0);
+    } else { // parent
+        sleep(1);
+        ASSERT_EQ(text, textFromSharedMemory());
+    }
+}
